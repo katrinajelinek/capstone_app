96f095ceb0c2b2a9b78b845628193bbc7752bea2
katrinajelinek <kmrjelinek@gmail.com>
2020-10-20 14:56:47 -0600

    seeds more data

--- db/seeds.rb
+++ db/seeds.rb
@@ -26,7 +26,22 @@
 # ])
 
 # Post.create!([
-#   { plant_type: "", trade_for: "", description: "", location: "", user_id: 1 },
+#   { plant_type: "Snake Plant", trade_for: "Spider Plant", description: "Healthy, rooted snake plant", location: "Colorado Springs", user_id: 3 },
+#   { plant_type: "Ivy", trade_for: "Anything", description: "Just clipped this ivy vine. Anyone want it?", location: "Colorado Springs", user_id: 6 },
+#   { plant_type: "Neon Pothos", trade_for: "Anything", description: "Clipped these a month ago. Very healthy roots", location: "Colorado Springs", user_id: 7 },
+#   { plant_type: "Spider Plant", trade_for: "Pothos", description: "I cut off the babies. Any takers? I'd love a pothos to trade.", location: "Colorado Springs", user_id: 8 },
+#   { plant_type: "Aloe", trade_for: "Anything", description: "My daughter knocked off a huge leg of my aloe. Would love to trade it for something", location: "Colorado Springs", user_id: 9 },
+#   { plant_type: "Christmas Cactus", trade_for: "Christmas Cactus", description: "I have a huge christmas cactus and would love to trade a fresh clipping for a different type. Mind has red flowers", location: "Colorado Springs", user_id: 10 },
+#   { plant_type: "Zebra Plant", trade_for: "Cactus", description: "Zebra Plant", location: "Colorado Springs", user_id: 11 },
+#   { plant_type: "Philodendron", trade_for: "Anything", description: "I've been propogating this guy for a week now.", location: "Colorado Springs", user_id: 11 },
+#   { plant_type: "Common Ivy", trade_for: "ZZ Plant", description: "I've been searching for a Raven ZZ Plant. Anyone want some common ivy for it?", location: "Colorado Springs", user_id: 13 },
+#   { plant_type: "Elephant Bush", trade_for: "Anything", description: "I'd be willing for propogate for a good trade offer. Maybe a cactus of some kind?", location: "Colorado Springs", user_id: 14 },
+#   { plant_type: "Cactus", trade_for: "Umbrella Plant", description: "Umbrella Tree", location: "Colorado Springs", user_id: 15 },
+#   { plant_type: "Golden Pothos", trade_for: "Fern", description: "The propogated vine looks a little sickly. Anyone still want it?", location: "Colorado Springs", user_id: 6 },
+#   { plant_type: "Wandering Dude", trade_for: "Anything", description: "Trying to trim my wandering dude and want the clippings to go to a good home.", location: "Colorado Springs", user_id: 17 },
+#   { plant_type: "Monstera", trade_for: "Swiss Monstera", description: "Really want a Swiss Monstera to trade. I'd be willing to cut a branch off my giant monstera", location: "Colorado Springs", user_id: 18 },
+#   { plant_type: "Peacelily", trade_for: "Aloe", description: "Just trying to get rid of my peacelily", location: "Colorado Springs", user_id: 9 },
+#   { plant_type: "Nerve Plant", trade_for: "Anything", description: "Nerve Plant", location: "Colorado Springs", user_id: 20 },
 # ])
 
 # Offer.create!([
