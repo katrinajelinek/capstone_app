bffa5748b0336bfe6279be536a7dc71923245aea
katrinajelinek <kmrjelinek@gmail.com>
2020-11-17 23:25:45 -0700

    changes form layout and users show layout

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,7 +6,7 @@ class Post < ApplicationRecord
 
   validates :plant_type, presence: true
   validates :location, presence: true
-  validates :description, length: { maximum: 100 }
+  validates :description, length: { maximum: 400 }
 
   def offer_accepted?
     offers.find_by(accepted: true)
